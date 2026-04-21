fc6752884e599d1ae4ca213b4742fc5dedd603e9
MangoCats <mangocats@gmail.com>
2026-04-21 09:20:24 -0400

    time format

--- server/src/modules/clock.rs
+++ server/src/modules/clock.rs
@@ -1,23 +1,43 @@
 use std::time::Duration;
-use chrono::Timelike;
-use crate::font::{draw_text, measure_text};
+use chrono::{Datelike, Timelike};
+use crate::font::draw_text;
 use crate::image::{E6Canvas, E6Color};
 use super::{Module, Rect};
 
-const CLOCK_SIZE_PX: f32 = 220.0;
+const SIZE_PX: f32 = 24.0;  // 5% of 480 px screen height
+const MARGIN:  i32 = 8;
+
+fn ordinal_suffix(day: u32) -> &'static str {
+    match (day % 100, day % 10) {
+        (11..=13, _) => "th",
+        (_, 1)       => "st",
+        (_, 2)       => "nd",
+        (_, 3)       => "rd",
+        _            => "th",
+    }
+}
 
 pub struct ClockModule;
 
 impl Module for ClockModule {
     fn render(&self, canvas: &mut E6Canvas, region: Rect) {
-        let now  = chrono::Local::now();
-        let text = format!("{:02}:{:02}", now.hour(), now.minute());
+        let now = chrono::Local::now();
+        let day = now.day();
+        let (_, hour12) = now.hour12();
+        let text = format!(
+            "{}, {} {}{} {} {}:{:02}:{:02} {}",
+            now.format("%A"),
+            now.format("%B"),
+            day,
+            ordinal_suffix(day),
+            now.year(),
+            hour12,
+            now.minute(),
+            now.second(),
+            if now.hour() < 12 { "AM" } else { "PM" },
+        );
 
-        let (text_w, ascent) = measure_text(&text, CLOCK_SIZE_PX);
-        let x = region.x + (region.width  - text_w) / 2;
-        let y = region.y + (region.height - ascent)  / 2;
-
-        draw_text(canvas, x, y, &text, CLOCK_SIZE_PX, E6Color::Black);
+        draw_text(canvas, region.x + MARGIN, region.y + MARGIN, &text, SIZE_PX, E6Color::Black);
     }
 
     fn data_refresh_interval(&self) -> Duration {
